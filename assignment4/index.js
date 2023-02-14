95124b1ba6d027f647d55151f7bb92b3d7fb4429
MIC <80168945+priyaankachoudhary12345@users.noreply.github.com>
2023-02-15 02:08:22 +0530

    updated
    updated file for corsera assignment 4

---
+++ assignment4/index.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Priyanka";
+names[1]="Juhii";
+names[2]="Vishal";
+names[3]="Shaker";
+names[4]="josh";
+names[5]="jovin";
+names[6]="Akshay";
+names[7]="jushi";
+names[8]="jams";
+names[9]="Kajol";
+
+for ( var i = 0; i<names.length; i++){
+if (names[i][0]=="j" || names[i][0] =="J") {
+console.log(" Goodbye "+ names[i])
+}
+else {
+    console.log("hello "+ names[i])
+}
+
+}
